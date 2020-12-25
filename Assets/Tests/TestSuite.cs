3c403401f7c34502e0a4d03120c1cf019225a68a
Don Crisman <crismand-dev@outlook.com>
2020-12-24 19:13:19 -0500

    Initial commit

---
+++ Assets/Tests/TestSuite.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class TestSuite
+    {
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator SimpleTestEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            float initialPos = 3.0f;
+            float newPos = 1.5f;
+
+            yield return new WaitForSeconds(0.1f);
+            
+            Assert.Less(newPos, initialPos);
+            
+        }
+    }
+}
